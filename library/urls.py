a1175ff17c650d7c79ebd4443a7e1a6107427098
Oksana <oksana.makarenko@iqvia.com>
2022-04-06 23:06:14 +0200

    user and order create form

--- library/urls.py
+++ library/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authors/', include('author.urls')),
     path('orders/', include('order.urls')),
+    path('users/', include('authentication.urls')),
+    path('book/', include('book.urls')),
     path('', include('book.urls'))
 ]
