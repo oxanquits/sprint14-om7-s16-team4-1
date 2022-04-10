2975e4837b15f41a93adf3d0fb4cf6cf6ff5f4d9
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-04-10 22:39:23 +0300

    fixed browser display

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from order.models import Order
 from author.models import Author
 from book.models import Book
 from . import serializers
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .serializers import OrderDetailSerializer
@@ -70,44 +70,58 @@ class BookViewSet(ModelViewSet):
         return serializers.BookDetailedSerializer
 
 
-class UserOrderViewSet(GenericViewSet):
+class UserOrderViewSet(ModelViewSet):
+    lookup_url_kwarg = 'order_id'
     serializer_class = OrderDetailSerializer
 
-    def list(self, request, user_id):
+    # def list(self, request, user_id):
+    #     user = get_object_or_404(User, id=user_id)
+    #     orders = self.get_serializer_class()(user.order_set, many=True).data
+    #     return Response(orders)
+    #
+    # def retrieve(self, request, user_id, order_id):
+    #     user, order = get_user_and_order(user_id, order_id)
+    #     return Response(self.get_serializer_class()(order).data)
+    #
+    # def create(self, request, user_id):
+    #     data = request.data
+    #     order = self.get_serializer_class()(data=data)
+    #     if order.is_valid(raise_exception=True):
+    #         order.save()
+    #         return Response(order.data, status=status.HTTP_201_CREATED)
+    #     # return Response(status=status.HTTP_400_BAD_REQUEST)
+    #
+    # def update(self, request, user_id, order_id):
+    #     user, order = get_user_and_order(user_id, order_id)
+    #     serializer = self.get_serializer_class()(instance=order, data=request.data)
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     # return Response({"detail": "Bad request."}, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # def destroy(self, request, user_id, order_id):
+    #     user, order = get_user_and_order(user_id, order_id)
+    #     order.delete()
+    #     return Response({"detail": "Successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
+    #
+
+    def get_queryset(self):
+        user_id, order_id = self.kwargs.get('user_id'), self.kwargs.get('order_id')
         user = get_object_or_404(User, id=user_id)
-        orders = self.get_serializer_class()(user.order_set, many=True).data
-        return Response(orders)
 
-    def retrieve(self, request, user_id, order_id):
-        user, order = get_user_and_order(user_id, order_id)
-        return Response(self.get_serializer_class()(order).data)
+        if order_id:
+            orders = user.order_set.filter(id=order_id)
+        else:
+            orders = user.order_set.all()
 
-    def create(self, request, user_id):
-        data = request.data
-        data.update({'user': user_id})
-        order = self.get_serializer_class()(data=data)
-        if order.is_valid(raise_exception=True):
-            order.save()
-            return Response(order.data, status=status.HTTP_201_CREATED)
-        # return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, user_id, order_id):
-        user, order = get_user_and_order(user_id, order_id)
-        serializer = self.get_serializer_class()(instance=order, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
-        # return Response({"detail": "Bad request."}, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, user_id, order_id):
-        user, order = get_user_and_order(user_id, order_id)
-        order.delete()
-        return Response({"detail": "Successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
+        if orders is None:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        return orders
 
 
-def get_user_and_order(user_id, order_id):
-    user = get_object_or_404(User, id=user_id)
-    order = user.order_set.filter(id=order_id).first()
-    if order is None:
-        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-    return user, order
+# def get_user_and_order(user_id, order_id):
+#     user = get_object_or_404(User, id=user_id)
+#     orders = user.order_set.filter(id=order_id)
+#     if order is None:
+#         return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+#     return orders
