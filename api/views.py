506e28091b49f6d44535848eb02f8a061a000af3
Yaroslav Kovaliv <80165970+morento101@users.noreply.github.com>
2022-04-11 11:04:47 +0300

    Removed useless code fragments

--- api/views.py
+++ api/views.py
@@ -1,5 +1,3 @@
-from rest_framework.response import Response
-from rest_framework import status
 from order.models import Order
 from author.models import Author
 from book.models import Book
@@ -74,37 +72,6 @@ class UserOrderViewSet(ModelViewSet):
     lookup_url_kwarg = 'order_id'
     serializer_class = OrderDetailSerializer
 
-    # def list(self, request, user_id):
-    #     user = get_object_or_404(User, id=user_id)
-    #     orders = self.get_serializer_class()(user.order_set, many=True).data
-    #     return Response(orders)
-    #
-    # def retrieve(self, request, user_id, order_id):
-    #     user, order = get_user_and_order(user_id, order_id)
-    #     return Response(self.get_serializer_class()(order).data)
-    #
-    # def create(self, request, user_id):
-    #     data = request.data
-    #     order = self.get_serializer_class()(data=data)
-    #     if order.is_valid(raise_exception=True):
-    #         order.save()
-    #         return Response(order.data, status=status.HTTP_201_CREATED)
-    #     # return Response(status=status.HTTP_400_BAD_REQUEST)
-    #
-    # def update(self, request, user_id, order_id):
-    #     user, order = get_user_and_order(user_id, order_id)
-    #     serializer = self.get_serializer_class()(instance=order, data=request.data)
-    #     if serializer.is_valid(raise_exception=True):
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     # return Response({"detail": "Bad request."}, status=status.HTTP_400_BAD_REQUEST)
-    #
-    # def destroy(self, request, user_id, order_id):
-    #     user, order = get_user_and_order(user_id, order_id)
-    #     order.delete()
-    #     return Response({"detail": "Successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
-    #
-
     def get_queryset(self):
         user_id, order_id = self.kwargs.get('user_id'), self.kwargs.get('order_id')
         user = get_object_or_404(User, id=user_id)
@@ -114,14 +81,4 @@ class UserOrderViewSet(ModelViewSet):
         else:
             orders = user.order_set.all()
 
-        if orders is None:
-            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
         return orders
-
-
-# def get_user_and_order(user_id, order_id):
-#     user = get_object_or_404(User, id=user_id)
-#     orders = user.order_set.filter(id=order_id)
-#     if order is None:
-#         return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-#     return orders
